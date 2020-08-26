a750fe95dddb9704ef07c14adc18075154f64c17
Genna Mergola <gennamergola@gmail.com>
2020-08-25 17:14:40 -0700

    updating deployment in readme, tests are WIP

--- src/Question.test.js
+++ src/Question.test.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { renderWithRouter } from './testHelper';
-import { waitForElementToBeRemoved } from "@testing-library/react";
 import Question from './Question';
 import * as apiFns from './apiHelpers';
 
 const MOCKED_QUESTION_DATA = { question: "question", answers: [{ id: 1, answer: 'answer', vote: 456, options: ['option'] }] };
-const MOCKED_QUESTIONS_DATA = { questions: [{ "question_main": "one", "question": "question" }]};
 
 function setMocks() {
-  apiFns.getQuestions = jest.fn().mockReturnValue(MOCKED_QUESTIONS_DATA);
-  apiFns.getQuestion = jest.fn().mockReturnValue(MOCKED_QUESTION_DATA);
+  apiFns.default.getQuestion = jest.fn().mockReturnValue(MOCKED_QUESTION_DATA);
 }
 
 function resetMocks() {
-  apiFns.getQuestion.mockRestore();
-  apiFns.getQuestions.mockRestore();
+  apiFns.default.getQuestion.mockRestore();
 }
 
 describe("renders with mocked question", () => {
@@ -23,14 +19,11 @@ describe("renders with mocked question", () => {
   });
 
   it("renders without crashing", function () {
-    renderWithRouter(<Question />);
+    renderWithRouter(<Question />)
   });
 
-  it("matches snapshot", async function () {
-    const { asFragment, queryByText, debug} = renderWithRouter(<Question />);
-    //WIP TODO: fix this snap shot so it doesn't show Loading...
-    // await waitForElementToBeRemoved(() => queryByText('Loading...'));
-    debug();
+  it("matches snapshot", function () {
+    const { asFragment } = renderWithRouter(<Question />);
     expect(asFragment()).toMatchSnapshot();
   });
 
